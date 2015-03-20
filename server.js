94b3942948ad94743491b2337f0a20851b8b80df
Guido García <guido@tid.es>
2015-03-20 15:55:31 +0100

    random songs

--- server.js
+++ server.js
@@ -4,9 +4,29 @@ var express = require('express'),
 var app = express();
 app.disable('x-powered-by');
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
-})
+// There are some random grooveshark song ids
+var SONGS = ["41886219", "7hPoPr", "26889869", "41414245", "41339801", "41487696"];
+
+app.get('/ping', function (req, res) {
+  res.json({pong: true});
+});
+
+app.post('/events', function (req, res) {
+  // TODO magic goes here
+  res.json({});
+});
+
+app.get('/next', function (req, res) {
+  var song = SONGS[Math.floor(Math.random() * SONGS.length)];
+
+  res.json({
+    next: [song]
+  });
+});
+
+app.get('*', function (req, res) {
+  res.status(404).end();
+});
 
 var server = http.createServer(app);
 var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3300;
