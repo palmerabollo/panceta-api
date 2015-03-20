7c7918e597818ee6b231a87dcc2484ffea40d23f
Guido García <guido@tid.es>
2015-03-20 16:05:59 +0100

    select song on event

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.disable('x-powered-by');
 
 // There are some random grooveshark song ids
 var SONGS = ["41886219", "7hPoPr", "26889869", "41414245", "41339801", "41487696"];
+var nextSong;
 
 app.get('/ping', function (req, res) {
   res.json({pong: true});
@@ -13,15 +14,19 @@ app.get('/ping', function (req, res) {
 
 app.post('/events', function (req, res) {
   // TODO magic goes here
+  nextSong = SONGS[Math.floor(Math.random() * SONGS.length)];
   res.json({});
 });
 
-app.get('/next', function (req, res) {
-  var song = SONGS[Math.floor(Math.random() * SONGS.length)];
+app.get('/playlist', function (req, res) {
+  var response = {};
 
-  res.json({
-    next: [song]
-  });
+  if (nextSong) {
+    response.next = [nextSong];
+    nextSong = null;
+  }
+
+  res.json(response);
 });
 
 app.get('*', function (req, res) {
